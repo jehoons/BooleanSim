e9d4b8033a16acd5713ed53442c04629c9f0c505
Je-Hoon Song <song.jehoon@gmail.com>
2016-10-18 21:06:08 +0900

    Update contents

--- boolean3_addon/test_attr_cy.py
+++ boolean3_addon/test_attr_cy.py
@@ -1,18 +1,22 @@
 import json
+from os.path import exists
 from boolean3_addon import attr_cy
 
+modeltext = '''
+A= Random
+B= Random
+C= Random
+A*= A or C
+B*= A and C
+C*= not A or B
+'''
+
+if not exists('engine.pyx'):
+    attr_cy.build(modeltext)
+
+import pyximport; pyximport.install()
+
 def test_this():
-    text  = '''
-    A= Random
-    B= Random
-    C= Random
-    A*= A or C
-    B*= A and C
-    C*= not A or B
-    '''
-    attr_cy.build(text)
-
-    res = attr_cy.run(samples=1000, steps=30, debug=False, on_states=['A'])
-
+    res = attr_cy.run(samples=100000, steps=30, debug=False, on_states=['A'], \
+        progress=True)
     json.dump(res, open('output.json', 'w'), indent=4)
-
